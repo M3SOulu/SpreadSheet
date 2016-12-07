48b7823885a9e43bdf2c7e70a8d633799bb1ed34
somename <someemail>
2016-12-07 17:10:34 +0100

    besouro automatic message

--- src/es/upm/grise/tests/SheetTest.java
+++ src/es/upm/grise/tests/SheetTest.java
@@ -46,4 +46,10 @@ public class SheetTest {
 		spreadSheet.evaluate("L2");
 		Assert.fail("#Error.");
 	}
+	
+	@Test (expected = CircularReferenceException.class)
+	public void testFunctionEvaluateString() throws CircularReferenceException {
+		spreadSheet.evaluate("T76");
+		Assert.fail("#Error.");
+	}
 }
