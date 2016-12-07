8dee7d680d71b01c16cd8d3cee606df5d21ac7b1
somename <someemail>
2016-12-07 15:12:14 +0100

    besouro automatic message

--- src/es/upm/grise/Sheet.java
+++ src/es/upm/grise/Sheet.java
@@ -21,7 +21,7 @@ public class Sheet {
 	 * @param contents	Any String (a valid formula, or not)
 	 */
 	public void set(String cell, String contents) {
-		// implement me
+		cells.put(cell, contents);
 	}
 
 	/**
