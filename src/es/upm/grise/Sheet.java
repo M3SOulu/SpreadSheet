5d48762669fd8cf83cb80911e49fb5ca5e92f264
somename <someemail>
2016-12-07 17:21:59 +0100

    besouro automatic message

--- src/es/upm/grise/Sheet.java
+++ src/es/upm/grise/Sheet.java
@@ -46,7 +46,7 @@ public class Sheet {
 		if (array[0] == '=') {
 			String arg = this.get(cell).substring(1);
 			if (cells.containsKey(arg)) {
-				if (visitedCells.contains(arg)) {
+				if (visitedCells.contains(cell)) {
 					s = "#Circular";
 				} else {
 					visitedCells.add(cell);
