020be0961866f1ede682a3be1f040ca08390e381
somename <someemail>
2016-12-07 16:15:00 +0100

    besouro automatic message

--- src/es/upm/grise/Sheet.java
+++ src/es/upm/grise/Sheet.java
@@ -5,43 +5,54 @@ import java.util.HashMap;
 
 public class Sheet {
 
-	private HashMap <String, String> cells = new HashMap <String, String>();
-	private ArrayList <String> visitedCells;
+	private HashMap<String, String> cells = new HashMap<String, String>();
+	private ArrayList<String> visitedCells;
 
 	/**
-	 * @param cell	A String representing a cell (e.g. "A1", "XZ21", etc.)
-	 * @return		The cell's contents (e.g. "1", "=5", "=1+B3", "=1+(B3*4)", etc.)
+	 * @param cell
+	 *            A String representing a cell (e.g. "A1", "XZ21", etc.)
+	 * @return The cell's contents (e.g. "1", "=5", "=1+B3", "=1+(B3*4)", etc.)
 	 */
 	public String get(String cell) {
 		return cells.get(cell);
 	}
 
 	/**
-	 * @param cell		A String representing a cell (e.g. "A1", "XZ21", etc.)
-	 * @param contents	Any String (a valid formula, or not)
+	 * @param cell
+	 *            A String representing a cell (e.g. "A1", "XZ21", etc.)
+	 * @param contents
+	 *            Any String (a valid formula, or not)
 	 */
 	public void set(String cell, String contents) {
 		cells.put(cell, contents);
 	}
 
 	/**
-	 * @param cell	A String representing a cell (e.g. "A1", "XZ21", etc.)
-	 * @return		The evaluation of the cell's contents (e.g. if the cell contains "1"
-	 * 				then returns "1"; if the cell contains "=1+2" returns "3"; if the
-	 * 				cell contains "=1+B3", then a recursive evaluation is performed). If
-	 * 				the evaluation gives an incorrect value (e.g.: "=1/0") return "#Error".
-	 *				In case of circular references, return #Circular
-	 * @throws CircularReferenceException 
+	 * @param cell
+	 *            A String representing a cell (e.g. "A1", "XZ21", etc.)
+	 * @return The evaluation of the cell's contents (e.g. if the cell contains
+	 *         "1" then returns "1"; if the cell contains "=1+2" returns "3"; if
+	 *         the cell contains "=1+B3", then a recursive evaluation is
+	 *         performed). If the evaluation gives an incorrect value (e.g.:
+	 *         "=1/0") return "#Error". In case of circular references, return
+	 *         #Circular
+	 * @throws CircularReferenceException
 	 */
 	public String evaluate(String cell) {
-		try{
-			Integer.parseInt(this.get(cell));
-		}catch (NumberFormatException e) {
-			return "#Error";
+		String s = "";
+		char[] array;
+		array = this.get(cell).toCharArray();
+		if (array[0] == '\'' && array[array.length - 1] == '\'') {
+			s =this.get(cell).substring(0, array.length - 2);
+		} else {
+			try {
+				Integer.parseInt(this.get(cell));
+			} catch (NumberFormatException e) {
+				s="#Error";
+			}
+			s=this.get(cell);
 		}
-		if(true)
-			return "ciao";
-		return this.get(cell);
+		return s;
 	}
 
 }
