44f704d36d0e32901a89dd92bd6393fbc384a095
somename <someemail>
2016-12-07 15:18:34 +0100

    besouro automatic message

--- src/es/upm/grise/tests/SheetTest.java
+++ src/es/upm/grise/tests/SheetTest.java
@@ -2,8 +2,10 @@ package es.upm.grise.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import es.upm.grise.ComputationErrorException;
 import es.upm.grise.Sheet;
 
 public class SheetTest {
@@ -11,8 +13,9 @@ public class SheetTest {
 	Sheet spreadSheet = new Sheet();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws ComputationErrorException {
+		spreadSheet.set("A1", "1");
+		assertEquals("1",spreadSheet.evaluate("A1"));
 	}
 
 }
