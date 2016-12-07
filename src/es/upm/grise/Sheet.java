39637a90cc7403afe87538b55c561287f655f3b4
somename <someemail>
2016-12-07 17:28:56 +0100

    besouro automatic message

--- src/es/upm/grise/Sheet.java
+++ src/es/upm/grise/Sheet.java
@@ -79,7 +79,7 @@ public class Sheet {
 				}		
 				if (formula.charAt(i) == '+'){
 					if(((formula.charAt(i+1) >= 'A') && (formula.charAt(i+1) <= 'Z')))
-						total = "" + (operation + Integer.parseInt(evaluate(formula.substring(i+1))));
+						total = "" + (operation + Integer.parseInt(evaluate(formula.substring(++i))));
 					else
 						total = "" + (operation + Integer.parseInt("" + formula.charAt(++i)));
 				}
