94e7cc8e65c1fa50011e62a6302e2bea28e785e5
somename <someemail>
2016-12-07 15:26:31 +0100

    besouro automatic message

--- src/es/upm/grise/tests/SheetTest.java
+++ src/es/upm/grise/tests/SheetTest.java
@@ -2,13 +2,26 @@ package es.upm.grise.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import es.upm.grise.Sheet;
+
 public class SheetTest {
+	Sheet foglio;
+	
+	@Before
+	public void setUp() {
+		foglio = new Sheet();
+		foglio.set("A1", "1");
+		foglio.set("A2", "1+2");
+		foglio.set("A3", "1*2");
+		foglio.set("A4", "1/2");
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void evalutateCellOneValue() {
+		assertEquals(1, foglio.evaluate("A1"));
 	}
 
 }
