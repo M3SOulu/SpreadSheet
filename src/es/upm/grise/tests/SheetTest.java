906c74493e5fe8c9d980084fda4e63e6705d46f9
somename <someemail>
2016-12-07 15:10:17 +0100

    besouro automatic message

--- src/es/upm/grise/tests/SheetTest.java
+++ src/es/upm/grise/tests/SheetTest.java
@@ -1,10 +1,19 @@
 package es.upm.grise.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import es.upm.grise.Sheet;
+
 public class SheetTest {
+
+	private Sheet sheet;
+	@Before
+	public void setUp() {
+		sheet = new Sheet();
+	}
 
 	@Test
 	public void test() {
