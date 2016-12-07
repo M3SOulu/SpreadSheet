15b175419f84df11b1a2d50e64849e3314d91e50
somename <someemail>
2016-12-07 16:29:47 +0100

    besouro automatic message

--- src/es/upm/grise/Sheet.java
+++ src/es/upm/grise/Sheet.java
@@ -56,7 +56,6 @@ public class Sheet {
 					}
 					else if((content.charAt(i)>='A'||content.charAt(i)>='Z')&&(content.charAt(i+1)>='0'||content.charAt(i+1)>='9')){
 						a=Integer.parseInt(this.evaluate(""+content.charAt(i)+content.charAt(i+1)));
-						System.out.println("Prima della ric:"+a);
 						i++;
 					}
 				}
