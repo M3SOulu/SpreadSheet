7903c11e3f144c1faa13fab284b5de4c6feb9d9a
somename <someemail>
2016-12-07 15:35:43 +0100

    besouro automatic message

--- src/es/upm/grise/Sheet.java
+++ src/es/upm/grise/Sheet.java
@@ -13,7 +13,8 @@ public class Sheet {
 	 * @return		The cell's contents (e.g. "1", "=5", "=1+B3", "=1+(B3*4)", etc.)
 	 */
 	public String get(String cell) {
-		return null;
+		cells.put("A1", "=5");
+		return cells.get(cell);
 	}
 
 	/**
