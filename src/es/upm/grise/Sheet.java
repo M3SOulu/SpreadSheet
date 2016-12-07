2c6f874961c1c60a0c4afb33af3e9561ef366cc8
somename <someemail>
2016-12-07 16:47:51 +0100

    besouro automatic message

--- src/es/upm/grise/Sheet.java
+++ src/es/upm/grise/Sheet.java
@@ -70,7 +70,7 @@ public class Sheet {
 				}
 				else{
 					if(content.charAt(i)>='0'||content.charAt(i)>='9'){
-						if(num==2)
+						if(num==2 || num==4 || num==5)
 							b+=(int) (Character.getNumericValue(content.charAt(i))*Math.pow(10,i-offset-1));
 						else if(num==3)
 							b-=(int) (Character.getNumericValue(content.charAt(i))*Math.pow(10,i-offset-1));
