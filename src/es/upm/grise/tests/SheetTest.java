65e318af672b57f484ee46c7ccf0c6c6ceab4978
somename <someemail>
2016-12-07 15:29:42 +0100

    besouro automatic message

--- src/es/upm/grise/tests/SheetTest.java
+++ src/es/upm/grise/tests/SheetTest.java
@@ -4,11 +4,26 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import es.upm.grise.CircularReferenceException;
+import es.upm.grise.ComputationErrorException;
+import es.upm.grise.Sheet;
+
 public class SheetTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testError(){
+	//arrange
+	Sheet mySheet = new Sheet();	
+	String result = null;
+	//act
+	try {
+		result = mySheet.evaluate("1/0");
+	} catch (ComputationErrorException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}	
+	//assert
+	assertEquals("#Error",result);
 	}
 
 }
