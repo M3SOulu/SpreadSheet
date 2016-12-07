b933155293961898b2622e982076e422b832aa4f
somename <someemail>
2016-12-07 16:25:55 +0100

    besouro automatic message

--- src/es/upm/grise/tests/SheetTest.java
+++ src/es/upm/grise/tests/SheetTest.java
@@ -4,13 +4,20 @@ import static org.junit.Assert.*;
 
 import java.util.HashMap;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import es.upm.grise.Sheet;
 
 public class SheetTest {
 	private HashMap <String, String> cells = new HashMap <String, String>();
-	
+	 @Before
+	 public void test(){
+		 cells.put("A1","valid");
+		 cells.put("XZ21", "valid");
+		 cells.put("/A1", "not valid");
+		 cells.put(".21A", "not valid");
+	 }
 	
 	@Test
 	public void cellcontainsoneintegernumber() {
