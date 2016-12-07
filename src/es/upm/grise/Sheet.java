08f1817658252c5a95731910541d11daec3f0638
somename <someemail>
2016-12-07 15:02:21 +0100

    besouro automatic message

--- src/es/upm/grise/Sheet.java
+++ src/es/upm/grise/Sheet.java
@@ -13,7 +13,7 @@ public class Sheet {
 	 * @return		The cell's contents (e.g. "1", "=5", "=1+B3", "=1+(B3*4)", etc.)
 	 */
 	public String get(String cell) {
-		return null;
+		return cells.get(cell);
 	}
 
 	/**
@@ -21,7 +21,7 @@ public class Sheet {
 	 * @param contents	Any String (a valid formula, or not)
 	 */
 	public void set(String cell, String contents) {
-		// implement me
+		cells.put(cell, contents);
 	}
 
 	/**
