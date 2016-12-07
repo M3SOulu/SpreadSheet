85042c47d8737c38a503a2db7cb02266b0fa8ef2
somename <someemail>
2016-12-07 16:41:28 +0100

    besouro automatic message

--- src/es/upm/grise/Sheet.java
+++ src/es/upm/grise/Sheet.java
@@ -44,18 +44,19 @@ public class Sheet {
 		char[] array;
 		array0 = this.get(cell).toCharArray();
 		if (array0[0] == '=') {
-			array = this.get(cell).substring(1).toCharArray();
+			String a = this.get(cell).substring(1);
+			array = a.toCharArray();
 			if (array[0] == '\'' && array[array.length - 1] == '\'') {
-				if (this.get(cell).substring(1, array.length - 1).isEmpty())
-					s = "#Errora";
+				if (a.substring(1, array.length - 1).isEmpty())
+					s = "#Error";
 				else
-					s = this.get(cell).substring(1, array.length - 1);
+					s = a.substring(1, array.length - 1);
 			} else {
 				try {
 					Integer.parseInt(this.get(cell));
-					s = this.get(cell);
+					s = a;
 				} catch (NumberFormatException e) {
-					s = "#Errorb";
+					s = "#Error";
 				}
 
 			}
