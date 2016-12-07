f3b5eb4410e8a6d94a2f66a0f0df6cd380c20912
somename <someemail>
2016-12-07 15:26:21 +0100

    besouro automatic message

--- src/es/upm/grise/tests/SheetTest.java
+++ src/es/upm/grise/tests/SheetTest.java
@@ -1,14 +1,20 @@
 package es.upm.grise.tests;
+import es.upm.grise.*;
 
 import static org.junit.Assert.*;
+
+import java.util.HashMap;
 
 import org.junit.Test;
 
 public class SheetTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testContentCell() {
+		
+		HashMap <String, String> cells = new HashMap <String, String>();
+		String result = cells.get("A1");
+		assertEquals("=5", result);
 	}
 
 }
