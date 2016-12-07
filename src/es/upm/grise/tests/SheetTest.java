a3d5a2d2f6e23ce1c780b30b654f163932ae321a
somename <someemail>
2016-12-07 16:20:37 +0100

    besouro automatic message

--- src/es/upm/grise/tests/SheetTest.java
+++ src/es/upm/grise/tests/SheetTest.java
@@ -8,6 +8,8 @@ import java.util.HashMap;
 import org.junit.Test;
 
 public class SheetTest {
+	
+	HashMap <String, String> cells = new HashMap <String, String>();
 
 	@Test
 	public void testContentCell() {
@@ -16,6 +18,13 @@ public class SheetTest {
 		cells.put("A1", "=5");
 		String result = cells.get("A1");
 		assertEquals("=5", result);
+	}
+	
+	@Test
+	public void testNumber(){
+		cells.put("B1", "15");
+		int result = Integer.parseInt("15");
+		assertEquals(15, result);
 	}
 
 }
