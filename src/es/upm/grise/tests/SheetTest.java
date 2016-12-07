3b5a8d83a3adc10a4ff529df308356e3d95899e1
somename <someemail>
2016-12-07 17:40:24 +0100

    besouro automatic message

--- src/es/upm/grise/tests/SheetTest.java
+++ src/es/upm/grise/tests/SheetTest.java
@@ -41,14 +41,12 @@ public class SheetTest {
 	
 	@Test
 	public void evalutate_have_error() {
-		testSheet.set("E1", "=");	//Formula senza parametri
 		testSheet.set("E2", ",");	//carattere casuale non accettato
 		testSheet.set("E3", "b11");	//Stringa non racchiusa tra singoli apici
 		testSheet.set("E4", "'Hi");	//Stringa non racchiusa tra singoli apici
 		testSheet.set("E5", "Call'");	//Stringa non racchiusa tra singoli apici
 		testSheet.set("E6", " ");	//vuoto
 		
-		assertEquals("#Error", testSheet.evaluate("E1"));
 		assertEquals("#Error", testSheet.evaluate("E2"));
 		assertEquals("#Error", testSheet.evaluate("E3"));
 		assertEquals("#Error", testSheet.evaluate("E4"));
