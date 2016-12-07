e00306f4b9dca0917ec5e3d54fd9c422ab0585ed
somename <someemail>
2016-12-07 15:51:46 +0100

    besouro automatic message

--- src/es/upm/grise/Sheet.java
+++ src/es/upm/grise/Sheet.java
@@ -43,11 +43,11 @@ public class Sheet {
 			
 			//controllo dei caratteri non accettati
 			for(int i = 0; i < contentValue.length() && !exit ; i++){
-				if(contentValue.charAt(i) < '0' || contentValue.charAt(i) > '9' || contentValue.charAt(i) > '9')
-					result = contentValue;
-				else{
+				if(contentValue.charAt(i) < '0' || contentValue.charAt(i) > '9'){	
 					result = "#Error";
 					exit = true;
+				}else{
+					result = contentValue;
 				}
 			}
 
