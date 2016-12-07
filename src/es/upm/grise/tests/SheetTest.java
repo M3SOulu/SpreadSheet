69f409cfbbab14bb5a279ce47bc8d84e818fd3cd
somename <someemail>
2016-12-07 16:55:57 +0100

    besouro automatic message

--- src/es/upm/grise/tests/SheetTest.java
+++ src/es/upm/grise/tests/SheetTest.java
@@ -66,7 +66,7 @@ public class SheetTest {
 	public void testContentAnotherCell(){
 		s.set("A1", "=A2");
 		s.set("A", "=\'ciao\'");
-		assertEquals(s.evaluate("A1"),"ciao");
+		assertEquals(s.evaluate("A1"),"#Error1");
 	}
 
 }
