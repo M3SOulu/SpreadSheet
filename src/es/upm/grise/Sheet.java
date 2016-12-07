17239b1cfd8dc0c4d00e24f9bad3999a6dc87b5f
somename <someemail>
2016-12-07 17:20:12 +0100

    besouro automatic message

--- src/es/upm/grise/Sheet.java
+++ src/es/upm/grise/Sheet.java
@@ -68,7 +68,8 @@ public class Sheet {
 			formula.toUpperCase();
 			if(formula.charAt(0) >= 'A' && formula.charAt(0) <= 'Z')		
 				result = evaluate(formula);
-			
+			else
+				result = formula;
 		}
 		else
 			result = "#Error";
