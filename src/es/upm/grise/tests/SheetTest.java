30e2e2fc059e1f20e54ef6b38337ae4c772b4ad0
somename <someemail>
2016-12-07 15:58:35 +0100

    besouro automatic message

--- src/es/upm/grise/tests/SheetTest.java
+++ src/es/upm/grise/tests/SheetTest.java
@@ -2,17 +2,20 @@ package es.upm.grise.tests;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+
 import org.junit.Test;
 
 import es.upm.grise.Sheet;
 
 public class SheetTest {
+	private HashMap <String, String> cells = new HashMap <String, String>();
 	
-
+	
 	@Test
 	public void cellcontainsoneintegernumber() {
 		Sheet number = new Sheet();
-		assertEquals("1",cells.set("1"));
+		assertEquals("1",cells.get("1"));
 	}
 
 }
