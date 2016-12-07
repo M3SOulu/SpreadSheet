fbc183b94ce04afb64cb12fdaa32c53d5dade10e
somename <someemail>
2016-12-07 16:38:44 +0100

    besouro automatic message

--- src/es/upm/grise/Sheet.java
+++ src/es/upm/grise/Sheet.java
@@ -56,6 +56,7 @@ public class Sheet {
 					}
 					else if((content.charAt(i)>='A'||content.charAt(i)>='Z')&&(content.charAt(i+1)>='0'||content.charAt(i+1)>='9')){
 						visitedCells.add(""+content.charAt(i)+content.charAt(i+1));
+						System.out.println(visitedCells.get(0));
 						a=Integer.parseInt(this.evaluate(""+content.charAt(i)+content.charAt(i+1)));
 						i++;
 					}
